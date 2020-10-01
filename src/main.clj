fb26cec7bbf9c83bc2b72d596037dadc5f98a372
twhitcomb <tom_whitcomb@intuit.com>
2020-10-01 16:57:45 -0700

    handles parens

--- src/main.clj
+++ src/main.clj
@@ -2,12 +2,16 @@
     (:gen-class)
     (:require 
       [lexer :as l] 
+      [clojure.test :refer [is]]
       [pratt :as p]))
 
   (defn -main
     [& args]
-    (prn "running")
+    (prn "Parsing " (first args))
     (let [
-          lex ((l/new-lexer "123+456+789") :start)
+          lex ((l/new-lexer (first args)) :start)
           parser (p/new-parser lex)]
       (prn (parser 0))))
+
+(is (= ((pratt/new-parser ((lexer/new-lexer "1 + ( (2 * 4) - 3)") :start)) 0)
+       '(:add (:literal "1") (:subtract (:multiply (:literal "2") (:literal "4")) (:literal "3")))))
