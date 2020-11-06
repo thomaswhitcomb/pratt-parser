bd3543bbd9148dc538ecb618cd9dc4f5e2048ee9
twhitcomb <tom_whitcomb@intuit.com>
2020-11-06 15:12:59 -0800

    initial decorator use

--- src/pratt.clj
+++ src/pratt.clj
@@ -6,81 +6,99 @@
      [clojure.string :as s]
      ))
 
-(defn decorate-token [parser {t :type value :value :as all}]
-  (cond 
-    (= t :number) 
-    (merge all {:nud (fn [] (list :literal value ))}) 
+(defn decorate-number []
+  (fn [{value :value :as token}]
+    (merge token {:nud
+                  (fn [] (list :literal value ))})))
 
-    (= t :word) 
-    (merge all {:nud (fn [] (list :literal value ))}) 
+(defn decorate-word []
+  (fn [{value :value :as token}]
+    (merge token {:nud (fn [] (list :literal value ))})))
 
-    (= t :left-paren) 
-    (merge all {:nud (fn [] (let [e (parser 0)
-                                  l (parser)] 
+
+(def decorator {
+                :number  (decorate-number)
+                :word (decorate-word)
+                })
+
+(defn decorate-token [parser {t :type value :value :as token}]
+  (cond
+    (contains? decorator t)
+    ((t decorator) token)
+
+    ;(= t :word)
+    ;(merge token {:nud (fn [] (list :literal value ))})
+
+    (= t :left-paren)
+    (merge token {:nud (fn [] (let [e (parser 0)
+                                  l (parser)]
                               (if (= (:value (l :curr)) ")")
-                                (do 
-                                  (l :next) 
+                                (do
+                                  (l :next)
                                   e)
                                 (throw (Exception. "Missing right paren")))))})
 
-    (= t :right-paren) 
-    (merge all {:lbp 0})
-                
-    (= t :power) 
-    (merge all {:lbp 30 
-                :led (fn [left] 
-                       (let [right (parser (- 30 1))] 
-                         (list :power left right )))}) 
+    (= t :right-paren)
+    (merge token {:lbp 0})
 
-    (= t :add) 
-    (merge all {:lbp 10 
-                :led (fn [left] 
-                       (let [right (parser 10)] 
-                         (list :add left right )))}) 
+    (= t :power)
+    (merge token {:lbp 30
+                :led (fn [left]
+                       (let [right (parser (- 30 1))]
+                         (list :power left right )))})
 
-    (= t :subtract) 
-    (merge all {:lbp 10 
-                :nud (fn [] 
-                         (let [e (parser 100)] 
+    (= t :add)
+    (merge token {:lbp 10
+                :led (fn [left]
+                       (let [right (parser 10)]
+                         (list :add left right )))})
+
+    (= t :subtract)
+    (merge token {:lbp 10
+                :nud (fn []
+                         (let [e (parser 100)]
                            (list :neg e )))
-                :led (fn [left] 
-                       (let [right (parser 10)] 
-                         (list :subtract left right)))}) 
+                :led (fn [left]
+                       (let [right (parser 10)]
+                         (list :subtract left right)))})
 
-    (= t :multiply) 
-    (merge all {:lbp 20 
-                :led (fn [left] 
-                       (let [right (parser 20)] 
-                         (list :multiply left right )))}) 
+    (= t :multiply)
+    (merge token {:lbp 20
+                :led (fn [left]
+                       (let [right (parser 20)]
+                         (list :multiply left right )))})
 
-    (= t :divide) 
-    (merge all {:lbp 20 
-                :led (fn [left] 
-                       (let [right (parser 20)] 
-                         (list :divide left right)))}) 
+    (= t :divide)
+    (merge token {:lbp 20
+                :led (fn [left]
+                       (let [right (parser 20)]
+                         (list :divide left right)))})
 
-    (= t :eof) 
-    (merge all {:lbp 0 
-                :nud (fn [] 
-                       (list :eof))}) 
+    (= t :eof)
+    (merge token {:lbp 0
+                :nud (fn []
+                       (list :eof))})
 
-    :else 
-    all))
-                
+    :else
+    token))
+
 
 (defn new-parser [lexer]
-    (fn parser 
-      ([rbp] 
-       (let [t (decorate-token parser (lexer :curr_next)) 
-             left (:nud t)] 
-         (loop [left1 (left)] 
-;          (prn "processing " (lexer :curr)) 
-           (if (< rbp (:lbp (decorate-token parser (lexer :curr)))) 
-             (let [t (decorate-token parser (lexer :curr_next)) 
-                   left2 ((t :led) left1)] 
-               (recur left2)) 
+    (fn parser
+      ([rbp]
+       (let [t (decorate-token parser (lexer :curr_next))
+             left (:nud t)]
+         (loop [left1 (left)]
+;          (prn "processing " (lexer :curr))
+           (if (< rbp (:lbp (decorate-token parser (lexer :curr))))
+             (let [t (decorate-token parser (lexer :curr_next))
+                   left2 ((t :led) left1)]
+               (recur left2))
              left1))))
       ([] lexer)))
+
+(is (= '(:literal "456")
+       ((new-parser ((l/new-lexer "456") :start)) 0)))
 
 (is (= '(:add (:add (:literal "123") (:literal "456")) (:literal "789"))
        ((new-parser ((l/new-lexer "123+456+789") :start)) 0)))
