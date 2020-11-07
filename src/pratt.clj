2fc203048a305ff9122e18e22abdb7f76197f538
twhitcomb <tom_whitcomb@intuit.com>
2020-11-06 20:42:02 -0800

    added more tests

--- src/pratt.clj
+++ src/pratt.clj
@@ -26,9 +26,6 @@
     (contains? decorator t)
     ((t decorator) token)
 
-    ;(= t :word)
-    ;(merge token {:nud (fn [] (list :literal value ))})
-
     (= t :left-paren)
     (merge token {:nud (fn [] (let [e (parser 0)
                                   l (parser)]
@@ -102,5 +99,26 @@
 
 (is (= '(:add (:add (:literal "123") (:literal "456")) (:literal "789"))
        ((new-parser ((l/new-lexer "123+456+789") :start)) 0)))
+
+(is (= '(:multiply (:add (:literal "123") (:literal  "456")) (:literal "789"))
+       ((new-parser ((l/new-lexer "(123+456) *789") :start)) 0)))
+
+(is (= '(:add (:literal "123") (:multiply (:literal  "456") (:literal "789")))
+       ((new-parser ((l/new-lexer "123+ (456 *789)") :start)) 0)))
+
+(is (= '(:add (:literal "123") (:neg (:literal  "5")))
+       ((new-parser ((l/new-lexer "123 + -5") :start)) 0)))
+
+(is (= '(:add (:literal "123") (:power (:literal  "5") (:literal "4")))
+       ((new-parser ((l/new-lexer "123 + 5 ** 4") :start)) 0)))
+
+(is (= '(:power (:add (:literal "123") (:literal  "5")) (:literal "4"))
+       ((new-parser ((l/new-lexer "(123 + 5) ** 4") :start)) 0)))
+
+(is (= '(:power (:add (:literal "123") (:literal  "5")) (:divide (:literal "4") (:literal"2")))
+       ((new-parser ((l/new-lexer "(123 + 5) ** (4/2)") :start)) 0)))
+
+(is (= '(:add (:literal "77") (:power (:add (:literal "123") (:literal  "5")) (:divide (:literal "4") (:literal"2"))))
+       ((new-parser ((l/new-lexer "77 + ( (123 + 5) ** (4/2) )") :start)) 0)))
 
 (is (= '(:subtract (:add (:literal "123") (:multiply (:literal "456") (:literal "789"))) (:divide (:literal "5") (:literal "77"))) ((new-parser ((l/new-lexer "123+456*789-5/77") :start)) 0)))
